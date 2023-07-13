b43d395955ab535fa3085100e5166bf2ee5a2e55
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-07-13 10:58:47 +0500

    commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+
+namespace jaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jArr =
+           new int[4][] {
+
+               new int[] { 1,2,3,4,5 },
+                new int[] { 23 , 44,8,6},
+                new int[] {24,6,88},
+                new int[] { 55,67},
+
+
+
+            };
+
+
+            //Console.WriteLine(jArr[2][1]);
+
+            foreach (int[] arr in jArr)
+            {
+
+                foreach (int item in arr)
+                {
+
+                    Console.Write(item);
+                    Console.Write('\t');
+
+
+                }
+
+                Console.WriteLine('\n');
+
+            }
+
+            int[][] jaggedArr = new int[4][];
+
+            int len = 5;
+            for (int i = 0; i < 4; i++)
+            {
+                jaggedArr[i] = new int[len];
+                for (int j = 0; j < len; j++)
+                {
+                    Console.Write("Enter Integer : ");
+                    jaggedArr[i][j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                len -= 1;
+
+
+
+
+
+            }
+
+            foreach (int[] arr in jaggedArr)
+            {
+                foreach (int elem in arr)
+                {
+
+                    Console.Write(elem + "\t");
+
+                }
+
+                Console.WriteLine('\n');
+
+
+
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ JaggedArray/JaggedArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
